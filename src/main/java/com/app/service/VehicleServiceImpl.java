4c87f27945e8f4ff8ff230cd59b9c46712cdbc45
Amit Nilekar <amitnilekar52@gmail.com>
2023-08-17 17:08:05 +0530

    Vehicle update and get api created

--- src/main/java/com/app/service/VehicleServiceImpl.java
+++ src/main/java/com/app/service/VehicleServiceImpl.java
@@ -1,5 +1,8 @@
 package com.app.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.transaction.Transactional;
 
 import org.modelmapper.ModelMapper;
@@ -12,6 +15,10 @@ import com.app.dao.VehicleRepository;
 import com.app.dao.VehicleTypeRepository;
 import com.app.dto.AddVehicalDto;
 import com.app.dto.ApiResponse;
+import com.app.dto.ServiceLocationDto;
+import com.app.dto.ServiceLocationResponseDto;
+import com.app.dto.UpdateVehicleDto;
+import com.app.dto.VehicleResponseDto;
 import com.app.entities.ServiceLocationEntity;
 import com.app.entities.Vehicle;
 import com.app.entities.VehicleBrand;
@@ -59,16 +66,62 @@ public class VehicleServiceImpl implements VehicleService {
 		return new ApiResponse("Vehicle added");
 	}
 	
-	
 	@Override
 	public ApiResponse deleteVehicle(Long id) {
 		
 		Vehicle vehicle = vehicleRepo.findById(id).orElseThrow(()-> new RuntimeException("vehicle not found"));
-
-		vehicleRepo.delete(vehicle);
+		ServiceLocationEntity location = serviceLocationRepo.findById(vehicle.getServiceLocation().getId()).orElseThrow(()-> new RuntimeException("location not found"));
+		//vehicleRepo.delete(vehicle);
 		
+		location.removeVehicle(vehicle);
 		return new ApiResponse("vehicle deleted");
 		
 	}
+	
+	@Override
+	public List<VehicleResponseDto> getAllVehicles() {
+		List<Vehicle> vehicleList = vehicleRepo.findAll();
+		
+		vehicleList.forEach(v-> System.out.println(v.getServiceLocation()));
+	
+		List<VehicleResponseDto> vehicleListResponse = vehicleList.stream() //Stream<Emp>
+		.map(vehicle -> mapper.map(vehicle, VehicleResponseDto.class)) //Stream<DTO>
+		.collect(Collectors.toList());
+		
+		for(int i=0;i<vehicleList.size();i++) {
+			vehicleListResponse.get(i).setLocation(mapper.map(vehicleList.get(i).getServiceLocation(), ServiceLocationResponseDto.class));
+		}
+		vehicleListResponse.forEach(v-> System.out.println(v.getLocation()));
+		
+		return vehicleListResponse;
+	
+	}
+	
+	@Override
+	public ApiResponse updateVehicle(UpdateVehicleDto vehicleDto) {
+	
+		Vehicle vehicle = vehicleRepo.findById(vehicleDto.getId())
+				.orElseThrow(()-> new RuntimeException("Vehicle not found"));
+		ServiceLocationEntity newLocation = serviceLocationRepo.findById(vehicleDto.getServiceLocationId())
+												.orElseThrow(()-> new RuntimeException("location not found"));
+		ServiceLocationEntity oldLocation = serviceLocationRepo.findById(vehicle.getServiceLocation().getId())
+				.orElseThrow(()-> new RuntimeException("location not found"));
+		
+		vehicle.setFuelType(vehicleDto.getFuelType());
+		
+		if(oldLocation.getId()== newLocation.getId()) {
+				vehicleRepo.save(vehicle);
+		}
+		else {
+			
+			oldLocation.removeVehicle(vehicle);
+			newLocation.addVehicle(vehicle);
+		}
+		
+		
+		
+		return new ApiResponse("vehicle updated");
+	}
+	
 	
 }
