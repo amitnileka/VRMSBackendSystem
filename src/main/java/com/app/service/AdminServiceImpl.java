1482850575c47e6ccaf67b103a0d62b449487465
Vishal <vishalautade123@gmail.com>
2023-08-18 19:26:31 +0530

    admin functionality and User functionality added

--- src/main/java/com/app/service/AdminServiceImpl.java
+++ src/main/java/com/app/service/AdminServiceImpl.java
@@ -1,5 +1,55 @@
 package com.app.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.dao.AdminRepository;
+import com.app.dto.ApiResponse;
+import com.app.dto.CredentialsRequestDto;
+import com.app.dto.CredentialsResponseDto;
+import com.app.entities.AdminEntity;
+
+
+@Service
+@Transactional
 public class AdminServiceImpl implements AdminService {
 
+	@Autowired
+	private AdminRepository adminRepo;
+	
+	@Override
+	public CredentialsResponseDto getValidAdmin(CredentialsRequestDto loginAdminDto) {
+		String email = loginAdminDto.getEmail();
+		String password = loginAdminDto.getPassword();
+		AdminEntity admin = adminRepo.findByEmailAndPassword(email, password)
+										.orElseThrow(()-> new RuntimeException("Invalid Admin Email or Password"));
+		
+		return admin != null ? new CredentialsResponseDto(admin.getId(),"jwt","Sign In Successfull") : 
+			new CredentialsResponseDto(null,"Nojwt","Sign In Unsuccessfull");
+		
+	}
+	
+	@Override
+	public ApiResponse getValidAdminByEmail(String email) {
+		System.out.println(email);
+		AdminEntity admin = adminRepo.findByEmail(email)
+							.orElseThrow(() -> new RuntimeException("Invalid Admin Email"));
+				
+		System.out.println(admin);
+	   return admin != null ? new ApiResponse("Valid Admin") : new ApiResponse("Invalid Admin");
+	}
+	
+	@Override
+	public ApiResponse updateForgotPasswordOfAdmin(CredentialsRequestDto userDto) {
+		AdminEntity admin = adminRepo.findByEmail(userDto.getEmail())
+							.orElseThrow(() -> new RuntimeException("Invalid Admin Email"));
+		if(admin != null) {
+			admin.setPassword(userDto.getPassword());
+			return new ApiResponse("Password Added Successfully");
+		}else {
+			return new ApiResponse("Invalid Email");
+		}
+	}
 }
--- src/main/java/com/app/service/UserService.java
+++ src/main/java/com/app/service/UserService.java
@@ -6,16 +6,18 @@ import com.app.dto.ApiResponse;
 import com.app.dto.ChangePasswordDto;
 import com.app.dto.ProfileDto;
 import com.app.dto.RegisterUserDto;
-import com.app.dto.SigninRequestDto;
-import com.app.dto.SigninResponseDto;
+import com.app.dto.CredentialsRequestDto;
+import com.app.dto.CredentialsResponseDto;
 
 
 
 public interface UserService {
 	public ApiResponse addUser(RegisterUserDto registerUserDto);
-	public SigninResponseDto getValidUser(SigninRequestDto loginUserDto);
+	public CredentialsResponseDto getValidUser(CredentialsRequestDto loginUserDto);
 	public List<ProfileDto> getUsers();
 	public ApiResponse editUserProfile(ProfileDto profile);
 	public ProfileDto getUserById(Long id);
 	public ApiResponse changePassword(ChangePasswordDto passDto);
+	public ApiResponse getValidUserByEmail(String email);
+	public ApiResponse updateForgotPasswordOfUser(CredentialsRequestDto userDto);
 }
